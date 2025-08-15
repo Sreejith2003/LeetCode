7c252893053a703f650855020204e82973fca700
Sreejith <sreejith20off@gmail.com>
2025-08-15 11:18:33 +0530

    3438.py updated

--- 3438.py
+++ 3438.py
@@ -1,2 +1,17 @@
 def findValidPair(s):
-    new_lst = list(s)
+    s = list(s)
+
+    lst = []
+    for x in s:
+        lst.append(int(x))
+    print(lst)
+
+    l = 0
+    for r in range(1, len(lst)):
+        if lst[l] + 1 == lst[r]:
+            return f"{lst[l]}{lst[r]}"
+        else:
+            continue
+
+s = "2523533"
+print(findValidPair(s))
